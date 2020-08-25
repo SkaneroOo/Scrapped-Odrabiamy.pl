23b0ac91691f8b0cbe1a5968a99bd9f8796e0b09
SkaneroOo <40175161+SkaneroOo@users.noreply.github.com>
2020-08-25 13:36:02 +0200

    .

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import pyautogui
+from pynput import keyboard
 import os
 
 a=int(input('1'))
@@ -7,31 +8,31 @@ c=int(input('3'))
 d=0
 
 
-
-while True:
-	e=input()
-	if len(e)==0:
+def on_press(key):
+	global a, b, c, d
+	if str(key) == "Key.space":
 		try:
 			os.makedirs(f"D:/backup C/skripty/ODRABIAMY SCRAPER XD/{a}/{b}/{c}")
 		except:
 			pass
 		pyautogui.screenshot(f"./{a}/{b}/{c}/{d}.png", region=(400, 125, 780, 915))
 		d+=1
-	elif len(e)==1:
+	elif str(key) == "'1'":
 		d=0
 		c+=1
-	elif len(e)==2:
+	elif str(key) == "'2'":
 		d=0
 		c=1
 		b+=1
-	elif len(e)==3:
+	elif str(key) == "'3'":
 		d=0
 		c=1
 		b=1
 		a+=1
+	print(f"{a}/{b}/{c}/{d}")
 	
 
+with keyboard.Listener(
+        on_press=on_press) as listener:
+    listener.join()
 
-
-
-pyautogui.screenshot(f"./{a}/{c}/{c}/{d}.png", region=(400, 125, 780, 915))
